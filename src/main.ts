345137944dce62df144aa6168754e7dcab0b67e7
chaleco <chaleconetworking@gmail.com>
2025-02-14 16:47:20 -0300

    feat: Onest Font
    Se añade Onest Font al proyecto

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+// Supports weights 100-900
+import '@fontsource-variable/onest';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
