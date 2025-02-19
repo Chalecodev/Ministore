94f9428968e08e129415e82176c2167bb8c14c17
Esteban-Riquelme <estebanriquelme605@gmail.com>
2025-02-18 22:12:14 -0300

    feat/conexion Api
    se realizo la conexion a la api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../components/navbar/navbar.component';
 import { HomeComponent } from "../components/home/home.component";
+import { HttpClient, HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { bootstrapApplication } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, NavbarComponent],
+  imports: [RouterOutlet, HomeComponent, NavbarComponent, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Ministore';
 }
+bootstrapApplication(AppComponent, {
+  providers: [provideHttpClient()] // Use provideHttpClient here
+});
--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { ProductCardComponent } from "../product-card/product-card.component";
+import { ApiService } from '../../app/services/api.service';
+import { IProduct } from '../../interface/iProduct';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +11,14 @@ import { ProductCardComponent } from "../product-card/product-card.component";
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  products: IProduct[] = [];
+  constructor(private apiService:ApiService) { }
+
+  ngOnInit() {
+    this.apiService.getProducts().subscribe((products)=>{
+      this.products = products;
+    });
+  }
 
 }
